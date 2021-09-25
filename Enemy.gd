f15430e0a89282355c996d0a173501cf08032bb9
gracerichardson01 <80234244+gracerichardson01@users.noreply.github.com>
2021-09-24 22:39:50 -0400

    .

---
+++ Enemy.gd
@@ -0,0 +1,33 @@
+
+extends KinematicBody2D
+
+var VP = Vector2.ZERO
+var direction = 0.0
+var dir_speed = 0.01
+var velocity = Vector2(2,0)
+
+var Bullets = null
+var Enemy_Bullet = preload("res://Enemy_Bullet/Enemy_Bullet.tscn")
+
+var Explosion = preload("res://Explosion/Explosion_ship.tscn")
+var Explosions = null
+
+
+func _ready():
+	VP = get_viewport().size
+	randomize()
+
+func _physics_process(_delta):
+	direction += dir_speed
+	while direction > 2*PI:
+		direction -= 2*PI
+
+func die():
+	if Explosions == null:
+		Explosions = get_node_or_null("/root/Game/Explosions")
+	if Explosions != null:
+		var explosion = Explosion.instance()
+		explosion.position = position
+		Explosions.add_child(explosion)
+	queue_free()
+
